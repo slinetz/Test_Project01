6c12cb063b26f570de6d9f0c41abfe536b604dbd
andy huang <andyhuang1979tw@gmail.com>
2023-01-09 14:37:10 -0800

    testing

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target
 #Input: [1,3,9,5,6,4], k = 8
 #Output: 1, 3
 def mathmatic_question(a:list, b:int) -> tuple: 
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     print(result)
 
     assert mathmatic_question(list_a, 0b1111111111111111111)
-    #assert mathmatic_question(["st"], 3), "the list should not contain string"
+    assert mathmatic_question(["st"], 3), "the list should not contain string"
